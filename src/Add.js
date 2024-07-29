40c401fd2979d9293ee8058354455887643ced6e
Meenakshi <meenakshiananthan8@gmail.com>
2024-07-29 11:58:38 +0530

    first Commit

---
+++ src/Add.js
@@ -0,0 +1,111 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App2.css";
+
+function Add() {
+  const [inputData, setInputData] = useState({
+    uname: "",
+    desg: "",
+    roomno: "",
+    
+  });
+
+  const naviget = useNavigate();
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    let result = validateValues(inputData);
+    // setSubmitting(true);
+
+    if (result === true) {
+      axios
+        .post("http://localhost:8086/User", inputData)
+        .then((res) => {
+          alert("Data added Successfully");
+          naviget("/");
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Please Enter the Valid Inputs!!!");
+    }
+  };
+
+  // validation Part for add user for student management system
+  // const [errors, setErrors] = useState({});
+  // const [submitting, setSubmitting] = useState(false);
+
+  const validateValues = (inputData) => {
+    if (inputData.uname.length === 0) {
+      alert("Please enter User Name !!! ");
+      return false;
+    } else if (inputData.desg.length === 0) {
+      alert("Please enter designation !!!");
+      return false;
+    } else if (inputData.roomno == 0) {
+      alert("Please enter roomno!!!");
+      return false;
+    } 
+     else {
+      return true;
+    }
+  };
+
+  return (
+    <div
+      id="add2"
+      className="d-flex w-100 vh-100 justify-content-center align-items-center "
+    >
+      <div className="w-50 border bg-light p-5">
+        <form onSubmit={handleSubmit}>
+          <h1>ADD USER DATA</h1>
+          <div>
+            <lable htmlFor="name">Name</lable>
+            <input
+              type="text"
+              name="uname"
+              className="form-control"
+              onChange={(e) =>
+                setInputData({ ...inputData, uname: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <lable htmlFor="Designation">Designation :</lable>
+            <input
+              type="text"
+              name="desg"
+              className="form-control"
+              onChange={(e) =>
+                setInputData({ ...inputData, desg: e.target.value })
+              }
+            />
+          </div>
+
+          <div>
+            <lable htmlFor="roomno">RoomNo :</lable>
+            <input
+              type="text"
+              name="roomno"
+              className="form-control"
+              onChange={(e) =>
+                setInputData({ ...inputData, roomno: e.target.value })
+              }
+            />
+          </div>
+
+         
+          <br />
+
+          <button className="btn btn-info ">Submit</button>
+        </form>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default Add;
---
+++ src/Edit.js
@@ -0,0 +1,90 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./App2.css";
+
+function Edit() {
+  const { uid } = useParams();
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log(uid);
+    axios
+      .get("http://localhost:8086/User/" + uid)
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    axios.put("http://localhost:8086/User", data).then((res) => {
+      alert("User Updated Successfully");
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <div
+        id="edit2"
+        className="d-flex w-100 vh-100 justify-content-center align-items-center "
+      >
+        <div className="w-50 border bg-light p-5">
+          <form onSubmit={handleSubmit}>
+            <h1>UPDATE USERS DATA'S</h1>
+            <div>
+              <lable htmlFor="id">ID :</lable>
+              <input
+                type="number"
+                disabled
+                name="uid"
+                className="form-control"
+                value={data.uid}
+                onChange={(e) => setData({ ...data, uid: e.target.value })}
+              />
+            </div>
+
+            <div>
+              <lable htmlFor="name">Name :</lable>
+              <input
+                type="text"
+                name="uname"
+                className="form-control"
+                value={data.uname}
+                onChange={(e) => setData({ ...data, uname: e.target.value })}
+              />
+            </div>
+            <div>
+              <lable htmlFor="DESIGNATION">Designation:</lable>
+              <input
+                type="text"
+                name="desg"
+                className="form-control"
+                value={data.desg}
+                onChange={(e) => setData({ ...data, desg: e.target.value })}
+              />
+            </div>
+
+            <div>
+              <lable htmlFor="roomno">Room No :</lable>
+              <input
+                type="text"
+                name="roomno"
+                className="form-control"
+                value={data.roomno}
+                onChange={(e) => setData({ ...data, roomno: e.target.value })}
+              />
+            </div>
+
+            
+            <br />
+
+            <button className="btn btn-info ">Update</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Edit;
